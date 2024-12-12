8e4db9da5fd494ccce1867ea3ea6b31c69a6fdbb
GabrielMussatto <128410250+GabrielMussatto@users.noreply.github.com>
2024-12-11 21:11:35 -0300

    Ajuste em livro

--- src/main/java/br/unitins/topicos1/model/livro/Livro.java
+++ src/main/java/br/unitins/topicos1/model/livro/Livro.java
@@ -71,6 +71,10 @@ public class Livro extends DefaultEntity {
         this.quantidadeEstoque -= quantidadeEstoque;
     }
 
+    public void aumentandoEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque += quantidadeEstoque;
+    }
+
     public String getTitulo() {
         return titulo;
     }
